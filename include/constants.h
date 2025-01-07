8c6c35c6774e7243e58d0a8262c79b1695b88a75
BSanduni <sanduni.herath@groupe-esigelec.org>
2025-01-07 16:10:54 +0100

    Made distance sensor work and moved all c files to src

--- include/constants.h
+++ include/constants.h
@@ -1,2 +1,16 @@
-#define PWM_FREQ 20 // kHz will break the duty cycle scale if changed which will require adding an expensive runtime multiplication
+/* =====================================================================================
+    User defined constants
+   ===================================================================================== */
+
+#define PWM_FREQ 20 // in kHz, will break the duty cycle scale if changed from 20 which will require adding an expensive runtime multiplication
+
+#define VOIE_CAPT_INFRA 0 // Voie du P1 sur laquelle le capteur infrarouge est branche
+#define OBSTACLE_DISTANCE_THRESH 4 // distance to stop from obstacle in cm
+
+
+/* =====================================================================================
+    Computed constants
+   ===================================================================================== */
+
 #define DOUBLE_PWM_PERIOD 200 / PWM_FREQ // Twice the period of PWM signal
+#define OBSTACLE_THRESH_RAW_ADC (2.5 / 1024)/(27.86/OBSTACLE_DISTANCE_THRESH + 0.42) // Seuil en valeur ADC brute
