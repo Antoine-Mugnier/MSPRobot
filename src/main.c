1492678b99b1a2ea5ec26049f57a41b446b563c0
omar <omar.zinelabidine@groupe-esigelec.org>
2025-01-10 10:54:20 +0100

    calculateur d'impulsions

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 #include <msp430g2553.h>
 #include <stdint.h>
-#include "motorFunctions.c"
+#include "motorFunctions.h"
+#include "calcul_impuls.h"
+#include "Afficheur.h"
 
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;  // Stop the watchdog timer
     BCSCTL1 = CALBC1_1MHZ;     // Set clock frequency to 1MHz
-    DCOCTL = CALDCO_1MHZ;      // Set clock frequency to 1MHz
+    DCOCTL = CALDCO_1MHZ;      // Set clock frequency to 1MHz 
 
-    motorInit();
-    setMotorParams(50, 50, 0b11);
-
-    while (1);                   // Enter infinite loop (timer is running autonomously)
+    encoderInit();
+    Aff_Init();
+    Aff_Efface();
+    __enable_interrupt();
+    
+    while (1){
+        Aff_valeur(encoderA_pulse_count);
+    }
+    
 }
