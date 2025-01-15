dc569d4ac1bcb846f9dfd7fb5d7ea441c6c2cffd
Anie <antoine.mugnier@groupe-esigelec.org>
2025-01-15 12:48:34 +0100

    unfinished test for previous changes

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include "constants.h"
 #include "motorFunctions.h"
 #include "ADC.h"
-#include "Afficheur.h"
+//#include "Afficheur.h"
+#include "utils.h"
 
 
 void main(void)
@@ -12,41 +13,39 @@ void main(void)
     BCSCTL1 = CALBC1_1MHZ;     // Set clock frequency to 1MHz
     DCOCTL = CALDCO_1MHZ;      // Set clock frequency to 1MHz 
 
+    __enable_interrupt();
+
+    progTimeInit();
     motorInit();
     ADC_init();
-    Aff_Init();
-    Aff_Luminosite(255);
-
-    setMotorParams(50, 50, 0b01);
     
-    //while (1);                   // Enter infinite loop (timer is running autonomously)
-    int res;
+    int g, d;
+    uint16_t oldTime = 0;
     while (1)
     {
-        ADC_Demarrer_conversion(VOIE_CAPT_INFRA);
-        res = ADC_Lire_resultat();
-        Aff_valeur(res);
-        if (res > 0x200)
-        {
-            setMotorParams(0, 0, 0b00);      // Stop motors
-        }
-        else
-        {
-            setMotorParams(50, 50, 0b01);    // Resume motor operation
-        }
+        if (time - oldTime >= 1) {
+            oldTime = time;
+            ADC_Demarrer_conversion(VOIE_CAPT_LIGNE_G);
+            g = ADC_Lire_resultat();
+            ADC_Demarrer_conversion(VOIE_CAPT_LIGNE_D);
+            d = ADC_Lire_resultat();
 
+            if (g < 0x3f0)
+            {
+                setMotorParams(100, 0b101);
+            }
+            else if (g > 0x260)
+            {
+                setMotorParams(100, 0b100);
+            }
+            if (d < 0x3f0)
+            {
+                setMotorParams(100, 0b011);
+            }
+            else if (d > 0x260)
+            {
+                setMotorParams(100, 0b010);
+            }
+        }
     }
 }
-
-/*
-// Function to convert ADC value to distance in cm
-float convertToDistance(uint16_t adcValue)
-{
-    float voltage = (adcValue * VREF) / ADC_MAX;  // Convert ADC value to voltage
-    // Approximate GP2D120 equation (voltage to distance), empirically derived
-    float distance = 27.86 / (voltage - 0.42);    // Distance formula approximation
-    if (distance > 30.0) distance = 30.0;         // Cap distance at 30cm
-    if (distance < 4.0) distance = 4.0;           // Cap distance at 4cm
-    return distance;
-} 
-*/
